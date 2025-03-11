ab1ae95ae9e9ef227077a2279b408198331885ff
khaledmo77 <khaledmo77@outlook.com>
2025-03-11 08:59:02 +0200

    Saving my local changes

--- obj/Debug/net8.0-windows/WareHouseProject.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/WareHouseProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WareHouseProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+842ba32ecc720c512136b030c9dded17b79c1a81")]
 [assembly: System.Reflection.AssemblyProductAttribute("WareHouseProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WareHouseProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
